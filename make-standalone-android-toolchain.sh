bf5832b8173181ee6227deffd520ed63ddb6bbcf
Nano <artiano@hotmail.com>
2017-03-06 23:17:10 +0800

    Update make-standalone-android-toolchain.sh

--- make-standalone-android-toolchain.sh
+++ make-standalone-android-toolchain.sh
@@ -1,1 +1,1 @@
-$NDK_ROOT/build/tools/make-standalone-toolchain.sh --platform=android-{VERSION} --install-dir={INSTALL_DIR} --toolchain=arm-linux-{TARGET_ARCH}-{VERSION} --stl=gnustl
+$NDK_ROOT/build/tools/make-standalone-toolchain.sh --platform=android-{PLATFORM_VERSION} --install-dir={INSTALL_DIR} --toolchain=arm-linux-{TARGET_ARCH}-{TOOLCHAIN_VERSION} --stl=gnustl
